7a3bbd8f3ac70dd9dfc72baa4249f2ddf62b07b6
Enigmi <nickygraydon@hotmail.com>
2016-02-08 13:15:00 +0000

    Corrected Circle center points to match given x/y coordinates Transitioned to using Graphics2D.

--- src/Core.java
+++ src/Core.java
@@ -1,4 +1,5 @@
 /**
+ * Personal version of the Risk assignment for COMP20050.
  * Created by ngraydon on 29/01/2016.
  */
 import javax.swing.*;
@@ -25,8 +26,6 @@ public class Core extends JFrame
 
     public static void main(String[] args)
     {
-
-
         Core gui = new Core();
         gui.setBackground(Color.black);
         gui.repaint();
@@ -35,54 +34,58 @@ public class Core extends JFrame
 
     public void paint(Graphics g)
     {
-        int x,y;
-        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        int x,y, radius = 35;
+        super.paint(g2);
 
         // Paint Countries
         for(int counter = 0; counter < Constants.NUM_COUNTRIES; counter++)
         {
             if(Constants.CONTINENT_IDS[counter] == 0)
             {
-                g.setColor(Color.yellow);
+                g2.setColor(Color.yellow);
             }
             else if (Constants.CONTINENT_IDS[counter] == 1)
             {
-                g.setColor(Color.blue);
+                g2.setColor(Color.blue);
             }
             else if (Constants.CONTINENT_IDS[counter] == 2)
             {
-                g.setColor(Color.green);
+                g2.setColor(Color.green);
             }
             else if (Constants.CONTINENT_IDS[counter] == 3)
             {
-                g.setColor(Color.cyan);
+                g2.setColor(Color.cyan);
             }
             else if (Constants.CONTINENT_IDS[counter] == 4)
             {
-                g.setColor(Color.orange);
+                g2.setColor(Color.orange);
             }
             else if (Constants.CONTINENT_IDS[counter] == 5)
             {
-                g.setColor(Colors.brown);
+                g2.setColor(Colors.brown);
             }
             else
             {
-                g.setColor(Color.pink);
+                g2.setColor(Color.pink);
             }
             x = Constants.COUNTRY_COORD[counter][0];
             y = Constants.COUNTRY_COORD[counter][1];
 
-            g.fillOval(x,y,35,35);
+            g2.fillOval(x-(radius/2),y-(radius/2),radius,radius);
+            // The reason it's x or y -(radius/2) is that if you just do filloval(x,y,radius,radius) the adjacent lines
+            // Drawn later will not line up with the circles center, as the way draw/fillOval works is by drawing
+            // An oval/circle inside a rectangle, so the center is displaced from the original co-ordinate given.
         }
 
-        g.setColor(Color.white);
+        g2.setColor(Color.white);
 
         //Paint Adjacent
         for(int counter = 0; counter < Constants.NUM_COUNTRIES; counter++)
         {
             for(int innerCounter = 0; innerCounter < Constants.ADJACENT[counter].length; innerCounter++)
             {
-                g.drawLine(Constants.COUNTRY_COORD[counter][0],Constants.COUNTRY_COORD[counter][1],Constants.COUNTRY_COORD[Constants.ADJACENT[counter][innerCounter]][0],Constants.COUNTRY_COORD[Constants.ADJACENT[counter][innerCounter]][1]);
+                g2.drawLine(Constants.COUNTRY_COORD[counter][0],Constants.COUNTRY_COORD[counter][1],Constants.COUNTRY_COORD[Constants.ADJACENT[counter][innerCounter]][0],Constants.COUNTRY_COORD[Constants.ADJACENT[counter][innerCounter]][1]);
             }
         }
 
